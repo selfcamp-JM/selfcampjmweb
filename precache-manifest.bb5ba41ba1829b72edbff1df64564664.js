f08b02739930150d982b472a9a459b457b19ee31
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-11 04:08:37 +0300

    Updates

---
+++ precache-manifest.bb5ba41ba1829b72edbff1df64564664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/selfcampjmweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cbcb6ec06d97136696e6",
+    "url": "/selfcampjmweb/static/js/runtime~main.cbcb6ec0.js"
+  },
+  {
+    "revision": "4c6805dafc86b9166114",
+    "url": "/selfcampjmweb/static/js/main.4c6805da.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/selfcampjmweb/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4c6805dafc86b9166114",
+    "url": "/selfcampjmweb/static/css/main.45fa6e3c.chunk.css"
+  },
+  {
+    "revision": "096d33b2ae19c621c8c191b81f53f3e5",
+    "url": "/selfcampjmweb/index.html"
+  }
+];
